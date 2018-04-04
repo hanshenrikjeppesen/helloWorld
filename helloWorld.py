3074bd216f85d3f832869a9a308c7612c1c65219
Hans Henrik Jeppesen <info@frist.dk>
2018-04-04 14:46:15 +0200

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# *****************************
+# *  Hello World script       *
+# *  Author: Hans@eaaa.dk     *
+# *  Version 1.0              *
+# *  Date: 04.04.18           *
+# *****************************
+
+print('Hello World!')
